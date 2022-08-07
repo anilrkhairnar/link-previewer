a52a7856e4637f4709a471ad4deba82d90f62c38
Anil Khairnar <anilrkhairnar@gmail.com>
2022-08-07 19:33:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import axios from "axios";
+
+import "./App.css";
+
+const App = () => {
+  const [img, setImg] = useState("");
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const options = {
+    method: "GET",
+    url: "https://og-link-preview.p.rapidapi.com/",
+    params: {
+      url: "https://dribbble.com/shots/15197978-Credit-Card-UI-challenge",
+    },
+    headers: {
+      "X-RapidAPI-Key": "bbc8ff2f91mshff933d9b5af0c6ap10faeejsn4278437b2a98",
+      "X-RapidAPI-Host": "og-link-preview.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      const result = response.data;
+      setImg(result.cover);
+      setTitle(result.title);
+      setDesc(result.description);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <img src={img} />
+      <h1>{title}</h1>
+      <p>{desc}</p>
     </div>
   );
-}
+};
 
 export default App;
